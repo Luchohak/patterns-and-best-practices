794e26ae84a18b33a72c2d877ea640bf30432ba7
Luchohak <a00958934@gmail.com>
2024-02-07 14:15:35 -0600

    Create additional context on AccordionItem to have a more granular control over id

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ function App() {
       <section>
         <h1>Why work with us? </h1>
         <Accordion className="accordion">
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title className="accordion-item-title" id="experience">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We got 20 years of xp
             </Accordion.Title>
-            <Accordion.Content
-              className="accordion-item-content"
-              id="experience"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>You can&apos;t go wrong with us.</p>
                 <p>
@@ -23,14 +20,11 @@ function App() {
               </article>
             </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item className="accordion-item">
-            <Accordion.Title className="accordion-item-title" id="local-guides">
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
               We're working with local guides
             </Accordion.Title>
-            <Accordion.Content
-              className="accordion-item-content"
-              id="local-guides"
-            >
+            <Accordion.Content className="accordion-item-content">
               <article>
                 <p>We are not doing this alone from our office.</p>
                 <p>
